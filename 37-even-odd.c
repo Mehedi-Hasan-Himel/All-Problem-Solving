414d3a22e6007d6f71a08dc4b863b077ce4eacb2
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2025-10-29 19:45:08 +0600

    37 even or odd

---
+++ 37-even-odd.c
@@ -0,0 +1,16 @@
+// Take a number from user and check if its a even number or odd number.
+
+#include <stdio.h>
+
+int main(){
+    int num;
+    scanf("%d", &num);
+
+    if(num%2 == 0){
+        printf("%d is Even Number\n", num);
+    }else{
+        printf("%d is Odd Number\n", num);
+    }
+
+    return 0;
+}
